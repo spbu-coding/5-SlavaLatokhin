08f9b44449262d628f51e9d31c4001b94e7e1202
SlavaLatokhin <st091713@student.spbu.ru>
2020-11-14 23:28:32 +0600

    Commit for address

---
+++ srs/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void other() {
+	printf("Top secret!\n");
+	printf("Slava was here!\n ");
+	printf("Congratulations!\n");
+    printf("You have entered in the secret function!\n");
+}
+
+void input() {
+	char buffer[30];
+	printf("Enter text:\n");
+	fflush(stdout);
+	scanf("%s", buffer);
+	printf("You entered: %s\n", buffer);
+	fflush(stdout);
+}
+
+int main() {
+	printf("Adress: %p\n", input);
+	printf("Adress: %p\n", other);
+	input();
+	return 0;
+}
